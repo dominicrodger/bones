4e03fa7e7cb071510119166dbfda4d6ec312c45b
Dominic Rodger <dominicrodger+github@gmail.com>
2015-12-09 17:07:07 +0000

    Removing irrelevant sections

--- functions.php
+++ functions.php
@@ -184,8 +184,8 @@ function bones_theme_customizer($wp_customize) {
 
   // $wp_customize->remove_section('title_tagline');
   // $wp_customize->remove_section('colors');
-  // $wp_customize->remove_section('background_image');
-  // $wp_customize->remove_section('static_front_page');
+  $wp_customize->remove_section('background_image');
+  $wp_customize->remove_section('static_front_page');
   // $wp_customize->remove_section('nav');
 
   // Uncomment the below lines to remove the default controls
